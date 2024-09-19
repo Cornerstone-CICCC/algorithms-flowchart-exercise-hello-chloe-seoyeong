4098784ec1cc03392737747b3dfab9042bf6197e
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-19 12:17:18 -0700

    Edited js answer for exercise 3

--- js/exercise3.js
+++ js/exercise3.js
@@ -2,16 +2,10 @@ let number1 = 45;
 let number2 = 93;
 let number3 = 10;
 
-if (number1 > number2) {
-  if (number1 > number3) {
-    console.log(`Largest number is ${number1}`);
-  } else {
-    console.log(`Largest number is ${number3}`);
-  }
-} else {
-  if (number2 > number3) {
-    console.log(`Largest number is ${number2}`);
-  } else {
-    console.log(`Largest number is ${number3}`);
-  }
+if (number1 > number2 && number1 > number3) {
+  console.log(`Largest number is ${number1}`);
+} else if (number2 > number1 && number2 > number3) {
+  console.log(`Largest number is ${number2}`);
+} else if (number3 > number1 && number3 > number2) {
+  console.log(`Largest number is ${number3}`);
 }
