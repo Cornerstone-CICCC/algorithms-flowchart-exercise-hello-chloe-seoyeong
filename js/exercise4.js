66233a31cfb1707d9ba6a1e474515e66daf74a46
Chole Seoyeong Ro <nostopzero@gmail.com>
2024-09-22 14:10:54 -0700

    "Refactoring exercise 4 flowchart and js"

--- js/exercise4.js
+++ js/exercise4.js
@@ -1,31 +1,51 @@
-let number1 = 5;
-let number2 = 3;
-let number3 = 10;
-let largest = null;
+// let number1 = 5;
+// let number2 = 3;
+// let number3 = 10;
 
-let triangle = `${number1}, ${number2}, ${number3} can be triangle! 🔺`
-let notnumber = `${number1}, ${number2}, ${number3} can not be triangle! ✖️`
+function checkTriangle(num1, num2, num3) {
+  let triangle = `${num1}, ${num2}, ${num3} can be triangle! 🔺`
+  let notTriangle = `${num1}, ${num2}, ${num3} can not be triangle! ✖️`
+  
+  let max = num1;
+  let sum = num1 + num2 + num3;
 
-if (number1 > number2 && number1 > number3) {
-  if (number1 <= number2 + number3) {
-    console.log(triangle);
-  } else {
-    console.log(notnumber);
+  if (max < num2) {
+    max = num2;
   }
-} else if (number2 > number1 && number2 > number3) {
-  if (number2 <= number1 + number3) {
-    console.log(triangle);
-  } else {
-    console.log(notnumber);
+  if (max < num3) {
+    max = num3;
   }
-} else if (number3 > number1 && number3 > number2) {
-  if (number3 <= number1 + number2) {
-    console.log(triangle);
-  } else {
-    console.log(notnumber);
+  if (max < sum - max) {
+    return triangle
   }
-} else if (number1 == number2 && number2 == number3) {
-  console.log(triangle);
-} else {
-  console.log(notnumber);
+  
+  return notTriangle;
 }
+
+console.log(checkTriangle(3,3,3));
+console.log(checkTriangle(10, 20, 30));
+console.log(checkTriangle(4, 5, 3));
+
+// if (number1 > number2 && number1 > number3) {
+//   if (number1 <= number2 + number3) {
+//     console.log(triangle);
+//   } else {
+//     console.log(notnumber);
+//   }
+// } else if (number2 > number1 && number2 > number3) {
+//   if (number2 <= number1 + number3) {
+//     console.log(triangle);
+//   } else {
+//     console.log(notnumber);
+//   }
+// } else if (number3 > number1 && number3 > number2) {
+//   if (number3 <= number1 + number2) {
+//     console.log(triangle);
+//   } else {
+//     console.log(notnumber);
+//   }
+// } else if (number1 == number2 && number2 == number3) {
+//   console.log(triangle);
+// } else {
+//   console.log(notnumber);
+// }
